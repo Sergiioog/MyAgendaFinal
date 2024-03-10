31389bb4ee3ccff57381316c7388258ceff3d224
sergiioog <sergiogargor9@gmail.com>
2024-03-10 19:17:03 +0100

    Carrusel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,39 @@
 import Image from "next/image";
 import OffCanvas from "./ui/OffCanvas";
+import { ImagesSlider } from "./ui/image-slider";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <section className="headerPrincipal h-[90px] min-h-[90px] border-b-[1px] flex justify-between items-center">
-      <figure className="w-30 max-h-full h-full flex justify-center items-center"> {/* Ajusta el tamaño del figure */}
-        <Image 
-          src="/assets/icons/logo.png" 
-          alt="logo" 
-          height={70}
-          width={70}
-          objectFit="cover" 
-          quality={100} 
-        />   
-        <h1 className="text-white ml-2">MyAgenda</h1>     
-      </figure>
-      <div className="bg-red-500">
-        <OffCanvas />
-      </div>
+        <figure className="w-30 max-h-full h-full flex justify-center items-center"> {/* Ajusta el tamaño del figure */}
+          <Image 
+            src="/assets/icons/logo.png" 
+            alt="logo" 
+            height={70}
+            width={70}
+            objectFit="cover" 
+            quality={100} 
+          />   
+          <h1 className="text-white ml-2">MyAgenda</h1>     
+        </figure>
+        <div className="flex justify-center items-center mr-3 ">
+          <OffCanvas />
+        </div>
       </section>
+      <section className="h-[300px] ">
+        <ImagesSlider
+          images={[
+            "/assets/images/carrusel1.jpeg",
+            "/assets/images/carrusel2.jpeg",
+            "/assets/images/carrusel3.jpeg"
+          ]}
+          autoplay={true}
+          direction='up'
+        />
+      </section>
+      
+
     </main>
   );
 }
