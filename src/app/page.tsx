2c92af472638a8b005a25ae81ea492757db8ab03
sergiioog <sergiogargor9@gmail.com>
2024-03-15 17:08:45 +0100

    Intento de Globo Terraqueo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import OffCanvas from "./ui/OffCanvas";
 import { ImagesSlider } from "./ui/image-slider";
 import PrimaryButton from "./ui/button1";
-
+import { World } from "./ui/globe";
 
 export default function Home() {
   return (
@@ -51,8 +51,34 @@ export default function Home() {
         </p>
         <div className="w-full flex justify-center items-center mt-5">
           <PrimaryButton />
-
         </div>
+      </section>
+      <section className="h-auto w-full relative mt-10">
+      <World
+  globeConfig={{
+    pointSize: 4,
+    globeColor: "#062056",
+    showAtmosphere: true,
+    atmosphereColor: "#FFFFFF",
+    atmosphereAltitude: 0.1,
+    emissive: "#062056",
+    emissiveIntensity: 0.1,
+    shininess: 0.9,
+    polygonColor: "rgba(255,255,255,0.7)",
+    ambientLight: "#38bdf8",
+    directionalLeftLight: "#ffffff",
+    directionalTopLight: "#ffffff",
+    pointLight: "#ffffff",
+    arcTime: 1000,
+    arcLength: 0.9,
+    rings: 1,
+    maxRings: 3,
+    initialPosition: { lat: 22.3193, lng: 114.1694 },
+    autoRotate: true,
+    autoRotateSpeed: 0.5
+  }}
+  data={[]}
+/>
       </section>
       
 
