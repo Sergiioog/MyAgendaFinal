8f02ed16d68df6198da3d2dc05258f0611453805
sergiioog <sergiogargor9@gmail.com>
2024-03-16 19:20:52 +0100

    Fin por hoy

--- src/app/ui/bentogrid.tsx
+++ src/app/ui/bentogrid.tsx
@@ -7,37 +7,37 @@ export default function BentoGrid() {
             <div className="item w-full h-auto  rounded-md relative overflow-hidden mb-3 flex justify-between flex-col hover:translate-x-1 transition-transform duration-500 ease-in-out hover:cursor-pointer">
               <div className="w-full h-8 flex justify-start items-center p-2 ">
                 <Image src={"assets/icons/calendarbento.svg"} alt="Imagen" height={20} width={20}/>
-                <h4 className="ml-[4px] text-sm font-bold">Agenda para guardar tus entrenamientos.</h4>
+                <h4 className="ml-[4px] text-sm font-bold text-neutral-300">Agenda para guardar tus entrenamientos.</h4>
               </div>
               <div className="w-full h-auto flex justify-start items-center font-semibold p-2 mt-[-7px]  ">
-                <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
+                <p className="text-xs text-neutral-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
               </div>
             </div>
             <div className="item w-full h-auto  rounded-md relative overflow-hidden mb-3 flex justify-between flex-col hover:translate-x-1 transition-transform duration-500 ease-in-out hover:cursor-pointer">
               <div className="w-full h-8 flex justify-start items-center p-2 ">
                 <Image src={"assets/icons/calendarbento.svg"} alt="Imagen" height={20} width={20}/>
-                <h4 className="ml-[4px] text-sm font-bold">Agenda para guardar tus entrenamientos.</h4>
+                <h4 className="ml-[4px] text-sm font-bold text-neutral-300">Agenda para guardar tus entrenamientos.</h4>
               </div>
               <div className="w-full h-auto flex justify-start items-center font-semibold p-2 mt-[-7px] ">
-                <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
+                <p className="text-xs text-neutral-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
               </div>
             </div>
             <div className="item w-full h-auto  rounded-md relative overflow-hidden mb-3 flex justify-between flex-col hover:translate-x-1 transition-transform duration-500 ease-in-out hover:cursor-pointer">
               <div className="w-full h-8 flex justify-start items-center p-2 ">
                 <Image src={"assets/icons/calendarbento.svg"} alt="Imagen" height={20} width={20}/>
-                <h4 className="ml-[4px] text-sm font-bold">Agenda para guardar tus entrenamientos.</h4>
+                <h4 className="ml-[4px] text-sm font-bold text-neutral-300">Agenda para guardar tus entrenamientos.</h4>
               </div>
               <div className="w-full h-auto flex justify-start items-center font-semibold p-2 mt-[-7px] ">
-                <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
+                <p className="text-xs text-neutral-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
               </div>
             </div>
             <div className="item w-full h-auto  rounded-md relative overflow-hidden mb-3 flex justify-between flex-col hover:translate-x-1 transition-transform duration-500 ease-in-out hover:cursor-pointer">
               <div className="w-full h-8 flex justify-start items-center p-2 ">
                 <Image src={"assets/icons/calendarbento.svg"} alt="Imagen" height={20} width={20}/>
-                <h4 className="ml-[4px] text-sm font-bold">Agenda para guardar tus entrenamientos.</h4>
+                <h4 className="ml-[4px] text-sm font-bold text-neutral-300">Agenda para guardar tus entrenamientos.</h4>
               </div>
               <div className="w-full h-auto flex justify-start items-center font-semibold p-2 mt-[-7px] ">
-                <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
+                <p className="text-xs text-neutral-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiquepprehenderit, eaque officiis? Ipsa, error?</p>
               </div>
             </div>
         </section>
