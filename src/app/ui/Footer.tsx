69b530f0a72ca152837051ce8b678eda81512d5e
sergiioog <sergiogargor9@gmail.com>
2024-03-20 15:39:22 +0100

    Footer editado

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -4,23 +4,23 @@ import Image from 'next/image';
 
 function FooterComponent() {
   return (
-    <Footer className='p-2 rounded-none mt-20 h-auto bg-slate-200 '>
+    <Footer className='p-2 footer mt-20 h-auto bg-transparent '>
      <div className="w-full text-center ">  {/* sm:flex sm:items-center sm:justify-between */}
      <div className='w-full flex flex-col sm:flex-row justify-center items-center mb-2 sm:justify-between '>
         <div className="flex justify-center items-center sm:flex sm:justify-center sm:items-center"> {/* Espaciado adicional entre logo y título en dispositivos medianos */}
           <Image src={"/assets/icons/logo.png"} alt={"logo"} height={50} width={50} />
-          <h3 className='font-bold text-2xl text-neutral-900'>MyAgenda</h3>
+          <h3 className='font-bold text-2xl text-neutral-400'>MyAgenda</h3>
         </div>
         <FooterLinkGroup className='mt-2 sm:ml-4 sm:mt-0 flex flex-row  sm:flex sm:justify-center sm:items-center'> {/* Cambio de dirección en dispositivos medianos */}
-          <FooterLink href="#">About</FooterLink>
-          <FooterLink href="#">Privacy Policy</FooterLink>
-          <FooterLink href="#">Licensing</FooterLink>
-          <FooterLink href="#">Contact</FooterLink>
+          <FooterLink href="#" className='text-neutral-400'>About</FooterLink>
+          <FooterLink href="#" className='text-neutral-400'>Privacy Policy</FooterLink>
+          <FooterLink href="#" className='text-neutral-400'>Licensing</FooterLink>
+          <FooterLink href="#" className='text-neutral-400'>Contact</FooterLink>
         </FooterLinkGroup>
       </div>
 
         <FooterDivider />
-        <FooterCopyright className=' h-auto mt-[-25px] flex justify-center items-center' href="#" by="MyAgenda" year={2024} />
+        <FooterCopyright className=' h-auto mt-[-15px] flex justify-center items-center text-neutral-400' href="#" by="MyAgenda" year={2024} />
       </div>
     </Footer>
   );
