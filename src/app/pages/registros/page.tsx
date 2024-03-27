ae8b9f5b0b867fcb578a87dfec0def27301c2eff
sergiioog <sergiogargor9@gmail.com>
2024-03-27 20:40:54 +0100

    Calendario finalizado estilo

--- src/app/pages/registros/page.tsx
+++ src/app/pages/registros/page.tsx
@@ -1,6 +1,5 @@
 import Header from "../../ui/header"
 import Example from "@/app/ui/calendar"
-import 'react-day-picker/dist/style.css';
 
 
 export default function MisRegistros() {
@@ -10,7 +9,7 @@ export default function MisRegistros() {
             <section className="h-auto w-full relative">
                 <h3 className="h3 text-center mt-10">Bienvenido a tu calendario, Sergio</h3>
                 <div id="container" className="p-6 flex justify-center items-center">
-                    <Example />
+                    <Example  />
                 </div>
             </section>
         </main>
--- src/app/ui/calendar.tsx
+++ src/app/ui/calendar.tsx
@@ -1,21 +1,31 @@
-"use client";
+"use client"
 import React from 'react';
 import { format } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
+import { es } from 'date-fns/locale';
+
 
 export default function Example() {
   const [selected, setSelected] = React.useState<Date>();
 
-  let footer = <p>Please pick a day.</p>;
+  let footer = <p>Por favor, seleccione un día</p>;
   if (selected) {
-    footer = <p>You picked {format(selected, 'PP')}.</p>;
+    footer = <p className='text-white mt-10'>Has elegido el {format(selected, 'PP')}.</p>;
   }
+
+
+
   return (
     <DayPicker
       mode="single"
       selected={selected}
       onSelect={setSelected}
       footer={footer}
+      locale={es}
+      modifiersStyles={{
+        selected: { backgroundColor: 'blue', color: 'white' },
+        today: { fontWeight: 'bold' }
+      }}
     />
   );
 }
